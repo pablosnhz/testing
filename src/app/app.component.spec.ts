135669d043df48d1a42df7b55b4e1c764abacdad
PabloSnhz <pablosanhuezaaa@gmail.com>
2024-11-15 19:50:21 -0300

    testing asynchronous

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,29 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+
+  let fixture: ComponentFixture<AppComponent>;
+  let el: DebugElement;
+  let component: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
-    }).compileComponents();
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(AppComponent);
+      el = fixture.debugElement;
+      component = fixture.componentInstance;
+    });
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  // it('should create the app', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app).toBeTruthy();
+  // });
 
   it(`should have the 'testing' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -20,10 +31,29 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('testing');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   expect(compiled.querySelector('h1')?.textContent).toContain('Hello, testing');
+  // });
+
+  it('should render a button with text "Subscribe" and the button should be disabled after clicked', () => {
+    component.isSubscribed = false;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, testing');
-  });
+    let btnElements = el.queryAll(By.css('.subscribe'));
+    console.log(btnElements);
+    // component.btnText = 'Subscribe';
+    btnElements[0].nativeElement.click();
+    fixture.detectChanges();
+    btnElements = el.queryAll(By.css('.subscribe'));
+    // con timeout
+    setTimeout(() => {
+      expect(btnElements[0].nativeElement.textContent).toBe('Subscribed');
+      expect(btnElements[0].nativeElement.disabled).toBeFalse();
+    }, 3000);
+    // sin timeout
+    // expect(btnElements[0].nativeElement.textContent).toBe('Subscribed');
+    // expect(btnElements[0].nativeElement.disabled).toBeTrue();
+  })
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ComponentsBasicComponent } from "./05-components-basic/components-basic/components-basic.component";
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ComponentsBasicComponent],
+  imports: [RouterOutlet, ComponentsBasicComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'testing';
+  btnText = 'Subscribe';
+  isSubscribed = false;
+
+  subscribe(){
+    setTimeout(() => {
+      this.isSubscribed = true;
+      this.btnText = 'Subscribed';
+    }, 3000);
+  }
 }
